3acbfa603fb903dd2851ae9497fa4e4c02684209
Donald Ephraim Curtis <dcurtis@milkbox.net>
2011-07-11 21:43:24 -0500

    Setting up for version 1 and import into marmalade.

--- hl-sentence.el
+++ hl-sentence.el
@@ -1,9 +1,37 @@
-;; (require 'hl-line)
+;;; hl-sentence.el --- highlight a sentence based on customizable face
+
+;; Copyright (c) 2011 Donald Ephraim Curtis
+
+;; Author: Donald Ephraim Curtis <dcurtis@milkbox.net>
+;; URL: http://github.com/milkypostman/hl-sentence
+;; Version: 1
+;; Keywords: highlighting
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Code:
 
 (setq hl-sentence-end "[^.].[.?!]+\\([]\"')}]*\\|<[^>]+>\\)\\($\\| $\\|\t\\| \\)[ \t\n]*")
 
 (setq hl-sentence-face (make-face 'hl-sentence-face))
-;; (set-face-attribute 'hl-sentence-face nil :foreground "#d5cfd9")
 
 (defun hl-sentence-begin-pos () (save-excursion (unless (= (point) (point-max)) (forward-char)) (backward-sentence) (point)))
 (defun hl-sentence-end-pos () (save-excursion (unless (= (point) (point-max)) (forward-char)) (backward-sentence) (forward-sentence) (point)))
@@ -33,3 +61,6 @@
 
 
 (provide 'hl-sentence)
+
+
+;;; hl-sentence.el ends here
